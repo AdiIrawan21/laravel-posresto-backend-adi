65ff92ed7e3b437f42c8f42912f4d0757e48ca6f
AdiIrawan21 <adiirawan01012003@gmail.com>
2024-05-25 05:50:05 +0700

    feat: updated categories

--- resources/views/pages/categories/index.blade.php
+++ resources/views/pages/categories/index.blade.php
@@ -12,9 +12,9 @@
         <section class="section">
             <div class="section-header">
                 <h1>Category</h1>
-                <div class="section-header-button">
+                {{-- <div class="section-header-button">
                     <a href="{{ route('categories.create') }}" class="btn btn-primary">Add New</a>
-                </div>
+                </div> --}}
                 {{-- <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Category</a></div>
